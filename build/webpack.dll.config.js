b4a46ac5c9d38931100cd80acad29d8f0d7fb5f2
yongqing_luo <953342912@qq.com>
2021-09-13 20:17:23 +0800

    feat: 配置初步完成

---
+++ build/webpack.dll.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const resolve = p => path.resolve(__dirname, p);
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const ora = require('ora');
+const chalk = require('chalk');
+
+let dllConfig = {
+  mode: 'production',
+  entry: {
+    vendor: ['vue']
+  },
+  output: {
+    path: resolve('../public/vendor'),
+    filename: '[name].dll.js',
+    library: '[name]_library' // 这里需要和webpack.DllPlugin中的`name: '[name]_library',`保持一致。
+  },
+  plugins: [
+    // 清除之前的dll文件
+    new CleanWebpackPlugin({
+      root: resolve('../public/vendor')
+    }),
+    // 定义插件
+    new webpack.DllPlugin({
+      path: resolve('../public/vendor/[name]-manifest.json'),
+      name: '[name]_library',
+      context: __dirname
+    })
+  ]
+};
+const spinner = ora('building dll...');
+spinner.start();
+
+webpack(dllConfig, (err, stats) => {
+  spinner.stop();
+  if (err) throw err;
+
+  process.stdout.write(
+    stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }) + '\n\n'
+  );
+
+  if (stats.hasErrors()) {
+    console.log(chalk.red('  Build failed with errors.\n'));
+    process.exit(1);
+  }
+});
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,8 +2,23 @@ const path = require('path');
 const resolve = p => path.resolve(__dirname, p);
 const commonConfig = require('./webpack.config');
 const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
-  devtool: 'source-map'
-})
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`)
+      `...`,
+      new CssMinimizerPlugin()
+    ]
+  }
+});
