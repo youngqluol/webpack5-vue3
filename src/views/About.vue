42770e0eef936e6d3401b07b6784356ca8a58601
lyq <953342912@qq.com>
2021-10-27 23:28:01 +0800

    feat: 完善首页/添加vuex/vue-router

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about-page">
+    This is about page.
+  </div>
+</template>
+<script>
+export default {
+  name: 'about',
+  setup() {}
+};
+</script>
+<style lang="less" scoped>
+  .about-page {
+    width: 5.4rem;
+    height: 5.4rem;
+    margin: 0 auto;
+    line-height: 5.4rem;
+    text-align: center;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="main-page">
+    <p>This is Home page</p>
+    <HelloWorld />
+    <div class="img2"></div>
+    <router-link to="/about">Go to ahout</router-link>
+    <button @click="addNum">{{ vuexNum }}</button>
+  </div>
+</template>
+<script>
+import {
+  ref, reactive, toRefs, computed
+} from 'vue';
+import { useStore } from 'vuex';
+import HelloWorld from '@/components/HelloWorld';
+
+export default {
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const num = ref(0);
+    const obj = reactive({ title: 1 });
+    const store = useStore();
+    const vuexNum = computed(() => store.state.num);
+    function fn() {
+      console.log('before promise');
+      return new Promise(resolve => {
+        setTimeout(() => {
+          console.log('promise');
+          resolve();
+        }, 2000);
+      });
+    }
+    async function promiseClick() {
+      await fn();
+      console.log('after promise');
+    }
+    return {
+      num,
+      vuexNum,
+      ...toRefs(obj),
+      promiseClick,
+      addNum: () => store.commit('increment')
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+  .main-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .img2 {
+      width: 3rem;
+      height: 3rem;
+      background-image: url('../assets/logo.png');
+      background-size: 3rem 3rem;
+    }
+  }
+</style>
